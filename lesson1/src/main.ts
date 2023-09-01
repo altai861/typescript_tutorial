ed6b2893dc7689916c19be04fa97efa09e02b1f1
altai861 <altaizandanbileg@gmail.com>
2023-09-01 09:17:24 +0800

    Added lesson13

--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -1,127 +1,21 @@
-class Coder {
-
-    secondLang!: string
-
-    constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
-        this.name = name
-        this.music = music
-        this.age = age
-        this.lang = lang
-    }
-
-    public getAge() {
-        return `Hello I'm ${this.age}`
-    }
+// Utility Types
+// Partial
+interface Assignment {
+    studentId: string,
+    title: string,
+    grade: number,
+    verified?: boolean,
 }
 
-const Altai = new Coder('Altai', 'Pop', 18)
-console.log(Altai.getAge())
-console.log(Altai.music)
-
-
-class WebDev extends Coder {
-    constructor(
-        public computer: string, 
-        name: string,
-        music: string,
-        age: number    
-    ) {
-        super(name, music, age)
-        this.computer = computer
-    }
-
-    public getLang() {
-        return `I write ${this.lang}`
-    }
+const updateAssignment = (assign: Assignment, propsTpUpdate: Partial<Assignment>): Assignment => {
+    return { ...assign, ...propsTpUpdate }
 }
 
-const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25)
-
-console.log(Sara.getLang())
-////////////////////////////////////
-
-interface Musician {
-    name: string,
-    instrument: string,
-    play(action: string): string
+const assign1: Assignment = {
+    studentId: 'compsci123',
+    title: "Final Project",
+    grade: 0,
 }
 
-class Guitarist implements Musician {
-    name: string
-    instrument: string
-
-    constructor(
-        name: string,
-        instrument: string
-    ) {
-        this.name = name
-        this.instrument = instrument
-    }
-
-    play(action: string) {
-        return `${this.name} ${action} the ${this.instrument}`
-    }
-}
-
-const Page = new Guitarist('Jimmy', 'guitar')
-
-console.log(Page.play('strums'))
-
-///////////////////////////////
-
-class Peeps {
-    static count: number = 0
-
-    static getCount(): number {
-        return Peeps.count
-    }
-
-    public id: number
-
-    constructor(public name: string) {
-        this.name = name
-        this.id = ++Peeps.count
-    }
-}
-
-
-const John = new Peeps('John')
-const Steve = new Peeps('Steve')
-const Amy = new Peeps('Amy')
-
-
-console.log(Amy.id)
-console.log(Steve.id)
-console.log(John.id)
-console.log(Peeps.count)
-
-/////////////////////////////////////
-
-class Bands {
-    private dataState: string[]
-
-    constructor() {
-        this.dataState = []
-    }
-
-    public get data(): string[] {
-        return this.dataState
-    }
-
-    public set data(value: string[]) {
-        if (Array.isArray(value) && value.every(element => typeof element === 'string')) {
-            this.dataState = value
-            return 
-        } else {
-            throw new Error('Param is not an array of strings')
-        }
-    }
-}
-
-const myBands = new Bands()
-
-myBands.data = ['Neil Young', 'Led Zep']
-console.log(myBands.data)
-
-myBands.data = [...myBands.data, 'ZZ Top']
-console.log(myBands.data)
+console.log(updateAssignment(assign1, { grade: 95 }))
+const assignGraded: Assignment = updateAssignment(assign1, {grade})
